aca961feb19006f25ce1db1d87056b86f9b1b298
Rostislav Provodenko <rostislav.provodenko@gmail.com>
2025-01-20 14:13:17 +0100

    wip

--- backend/src/e2e/trade-price.gateway.e2e.spec.ts
+++ backend/src/e2e/trade-price.gateway.e2e.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../app.module';
 import { INestApplication } from '@nestjs/common';
+import { io, Socket } from 'socket.io-client';
 
+jest.setTimeout(60000);
 describe('Trade price lifecycle', () => {
   let app: INestApplication;
+  let ingestSocket: Socket;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,7 +15,26 @@ describe('Trade price lifecycle', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    await app.listen(0);
+
+    ingestSocket = io(`${await app.getUrl()}/ingest-trade-price`);
+    await new Promise<void>((res, rej) => {
+      ingestSocket.on('connect', () => {
+        res();
+      });
+    });
+  });
+  afterEach(async () => {
+    ingestSocket.disconnect();
+    ingestSocket.close();
+    await app.close();
   });
 
-  it('should allow ingesting a trade price', () => {});
+  it('should allow ingesting a trade price', async () => {
+    ingestSocket.emit('trade-price', {
+      ticker: 'BTC',
+      priceUsd: 109123.21,
+      timestamp: new Date().getTime(),
+    });
+  });
 });
--- backend/src/trade-price-ingest/trade-price-ingest.gateway.ts
+++ backend/src/trade-price-ingest/trade-price-ingest.gateway.ts
@@ -1,14 +1,41 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
     origin: '*', // Adjust the origin for your use case
   },
-  path: '/receive-trade-price',
+  namespace: '/ingest-trade-price',
 })
-export class TradePriceIngestGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class TradePriceIngestGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    console.log('----herexxx');
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
+  handleConnection(client: any, ...args: any[]) {
+    const { sockets } = this.server.sockets;
+
+    console.log(sockets);
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit('onMessage', {
+      msg: 'New Message',
+      content: body,
+    });
   }
 }
